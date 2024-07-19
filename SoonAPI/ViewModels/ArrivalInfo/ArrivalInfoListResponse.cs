b4ff66377c396d3dc28b7b165c76fdf5be42555c
Kevin Perdomo <kevinperdomo.ag@gmail.com>
2024-07-19 12:18:30 -0700

    ArrivalInfo JSON response

---
+++ SoonAPI/ViewModels/ArrivalInfo/ArrivalInfoListResponse.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+public class ArrivalInfoListResponse : JsonResponse
+{
+    public List<ArrivalInfo> ArrivalInfos { get; set; }
+
+
+    public static ArrivalInfoListResponse Get()
+    {
+        ArrivalInfoListResponse r = new ArrivalInfoListResponse();
+        r.Status = 0;
+        r.ArrivalInfos = ArrivalInfo.Get();
+        return r;
+    }
+}
---
+++ SoonAPI/ViewModels/ArrivalInfo/ArrivalInfoResponse.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+public class ArrivalInfoResponse : JsonResponse
+{
+    public ArrivalInfo ArrivalInfo { get; set; }
+
+    public static ArrivalInfoResponse Get(ArrivalInfo b)
+    {
+        ArrivalInfoResponse r = new ArrivalInfoResponse();
+        r.Status = 0;
+        r.ArrivalInfo = b;
+        return r;
+    }
+}
+
