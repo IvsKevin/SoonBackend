c2c5cec2aa7292da4dd51e881323726f0990a598
Kevin Perdomo <kevinperdomo.ag@gmail.com>
2024-07-19 12:18:56 -0700

    Transaction JSON response

---
+++ SoonAPI/ViewModels/Transaction/TransactionListResponse.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+public class TransactionListResponse : JsonResponse
+{
+    public List<Transaction> Transactions { get; set; }
+
+
+    public static TransactionListResponse Get()
+    {
+        TransactionListResponse r = new TransactionListResponse();
+        r.Status = 0;
+        r.Transactions = Transaction.Get();
+        return r;
+    }
+}
---
+++ SoonAPI/ViewModels/Transaction/TransactionResponse.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+public class TransactionResponse : JsonResponse
+{
+    public Transaction Transaction { get; set; }
+
+    public static TransactionResponse Get(Transaction b)
+    {
+        TransactionResponse r = new TransactionResponse();
+        r.Status = 0;
+        r.Transaction = b;
+        return r;
+    }
+}
+
