e0ab0e15b7276fa92c29aba416a703d1a159cebc
Kevin Perdomo <kevinperdomo.ag@gmail.com>
2024-07-19 12:18:39 -0700

    Civil JSON response

---
+++ SoonAPI/ViewModels/Civil/CivilListResponse.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+public class CivilListResponse : JsonResponse
+{
+    public List<Civil> Civils { get; set; }
+
+
+    public static CivilListResponse Get()
+    {
+        CivilListResponse r = new CivilListResponse();
+        r.Status = 0;
+        r.Civils = Civil.Get();
+        return r;
+    }
+}
---
+++ SoonAPI/ViewModels/Civil/CivilResponse.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+public class CivilResponse : JsonResponse
+{
+    public Civil Civil { get; set; }
+
+    public static CivilResponse Get(Civil b)
+    {
+        CivilResponse r = new CivilResponse();
+        r.Status = 0;
+        r.Civil = b;
+        return r;
+    }
+}
+
