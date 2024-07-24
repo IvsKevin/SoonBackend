a31c5ce000bd05fc79e7eb4d994d941f81485f23
Kevin Perdomo <kevinperdomo.ag@gmail.com>
2024-07-23 19:14:03 -0700

    PostModel User added

---
+++ SoonAPI/Models/PostModels/PostUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public class PostUser
+{
+    public int? code { get; set; }
+    public string? email { get; set; }
+    public string? password { get; set; }
+    public int? type { get; set; }
+
+}
+
