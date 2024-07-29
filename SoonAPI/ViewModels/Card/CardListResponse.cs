3862fdd697d5650cc82a21e48e03c8bdb7965c8c
IvsKevin <kevinperdomo.ag@gmail.com>
2024-07-29 11:07:10 -0700

    Card response added

---
+++ SoonAPI/ViewModels/Card/CardListResponse.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+public class CardListResponse : JsonResponse
+{
+    public List<Card>? Cards { get; set; }
+
+
+    public static CardListResponse Get()
+    {
+        CardListResponse r = new CardListResponse();
+        r.Status = 0;
+        r.Cards = Card.Get();
+        return r;
+    }
+}
---
+++ SoonAPI/ViewModels/Card/CardResponse.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+public class CardResponse : JsonResponse
+{
+    public Card? Card { get; set; }
+
+    public static CardResponse Get(Card b)
+    {
+        CardResponse r = new CardResponse();
+        r.Status = 0;
+        r.Card = b;
+        return r;
+    }
+}
+
